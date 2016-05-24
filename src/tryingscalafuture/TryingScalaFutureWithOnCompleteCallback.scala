51c59c727ec10a7797e388b97599435facf4c569
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-05-24 09:34:02 +0530

    Use different ways to create future...
    1. One is how we can use map on a future to add computations after the point of time (fastTemperatureOkay)
       - fastTemperatureOkay is a simple computation and doesn't involve future i.e. can be completed then and there... there is no async element involved.
       - But because its mapped on a future, it simply returns a future of boolean.
    2. However slowTemperatureOkay is itself a future calculation...
       - Mapping that on heatWater (remember heatWater is itself Future[Water]) returns a Future which comprises of Future[Boolean]
       - And very soon this shall make no sense...
    3. What we finally want is one Future[Boolean] out of heating Water and checking its temperature to see if its within the range or not
    4. So we can use flatMap... or ofcourse
    5. for comprehension :)

--- src/tryingscalafuture/TryingScalaFutureWithOnCompleteCallback.scala
+++ src/tryingscalafuture/TryingScalaFutureWithOnCompleteCallback.scala
@@ -9,6 +9,8 @@ object TryingScalaFutureWithOnCompleteCallback extends App {
   type CoffeeBeans = String
   type GroundCoffee = String
 
+  case class Water(temperature: Int)
+
   case class GrindingException(msg:String) extends Exception(msg)
 
   def grind(beans: CoffeeBeans): Future[GroundCoffee] = Future {
@@ -24,5 +26,36 @@ object TryingScalaFutureWithOnCompleteCallback extends App {
     case Success(groundCoffee:GroundCoffee) => println("okay we got the grounded coffee!")
     case Failure(ex) => println("this grinder needs a serious replacement!")
   }
+
+  def heatWater(water: Water): Future[Water] = Future {
+    println("Heating water now...")
+    Thread.sleep(Random.nextInt(2000))
+    println("hot, its hot!")
+    water.copy(temperature = 84)
+  }
+
+  val fastTemperatureOkay: Future[Boolean] = heatWater(Water(10)).map { water =>
+    println("we are in the future")
+    (80 to 85).contains(water.temperature)
+  }
+
+  def slowTemperatureOkay(water: Water): Future[Boolean] = Future {
+    (80 to 85).contains(water.temperature)
+  }
+
+  val nestedFuture: Future[Future[Boolean]] = heatWater(Water(10)).map {
+    water => slowTemperatureOkay(water)
+  }
+
+  val flatFuture: Future[Boolean] = heatWater(Water(10)).flatMap {
+    water => slowTemperatureOkay(water)
+  }
+
+  val isAcceptable: Future[Boolean] = for {
+    h <- heatWater(Water(10))
+    ok <- slowTemperatureOkay(h)
+  } yield ok
+
+  Thread.sleep(3000)
 
 }
