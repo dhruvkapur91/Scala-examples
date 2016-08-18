2c1693e41bd1d1933162aa6c0303e17e91d8d844
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-08-18 11:44:28 +0530

    Add logic for parsing a letter

--- src/main/scala/parsercombinators/RAMToyLanguageParser.scala
+++ src/main/scala/parsercombinators/RAMToyLanguageParser.scala
@@ -5,13 +5,19 @@ import scala.util.parsing.combinator.RegexParsers
 class RAMToyLanguageParser extends RegexParsers {
   def digit: Parser[String] = "[0-9]".r
 
-//  def letter :
+  def letter: Parser[String] = "[a-z]|[A-Z]".r
 
-  def parseDigit(text: String) = parseAll(digit,text)
+  def parseDigit(text: String) = parseAll(digit, text)
+
+  def parseLetter(text: String) = parseAll(letter, text)
 }
 
 object RAMToyLanguageParser extends App {
   println {
     s"Should match a digit ${new RAMToyLanguageParser().parseDigit("1")} "
+  }
+
+  println {
+    s"Should match a letter ${new RAMToyLanguageParser().parseLetter("A")} "
   }
 }
