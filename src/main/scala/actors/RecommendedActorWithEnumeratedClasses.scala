1b7ec402798b1dce0ee6aa7d65128e4f23b6a8c5
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-08-31 13:57:55 +0530

    Add recommended actor with messages enumerated inside companion object

---
+++ src/main/scala/actors/RecommendedActorWithEnumeratedClasses.scala
@@ -0,0 +1,29 @@
+package actors
+
+import actors.RecommendedActorWithEnumeratedClasses.{GoodBye, Greeting, createRecommendedActor}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+
+class RecommendedActorWithEnumeratedClasses(greeting : String) extends Actor with ActorLogging {
+
+  override def receive: Receive = {
+    case Greeting(msg) if msg.matches(greeting) => log.info(greeting)
+    case GoodBye => log.info("So long and thanks for all the fish!")
+    case _  => log.info("Unexpected message found!!")
+  }
+}
+
+object RecommendedActorWithEnumeratedClasses {
+  def createRecommendedActor(greeting : String) = Props(classOf[RecommendedActorWithEnumeratedClasses],greeting)
+
+  case class Greeting(msg : String)
+  case object GoodBye
+}
+
+object RecommendedActorWithEnumeratedClassesRunner extends App {
+  val actorSystem = ActorSystem.create("NewActorSystem")
+  private val recommendedActor: ActorRef = actorSystem.actorOf(createRecommendedActor("YAYY!"))
+  recommendedActor ! Greeting("YAYY!")
+  recommendedActor ! "No YAYY!"
+  recommendedActor ! GoodBye
+  actorSystem.terminate()
+}
